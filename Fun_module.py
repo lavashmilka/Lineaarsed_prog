b5204666ff6ad39a3f0477152a12b52c1d3cc81b
opilane <opilane@HP-CZC2349HTM>
2025-03-27 10:53:53 +0200

    .

--- Fun_module.py
+++ Fun_module.py
@@ -1,14 +1,20 @@
 from Teema5FunktsioonidDef import *
+while True:
+    try:
+        a=float(input("Arv1: "))
+        b=float(input("Arv2: "))
+        t=input("Tehe: ")
+        if t.lowe()=="exit":
+              print("Exit programmist")
+              break
+        vastus=arithmetic(a,b,t)
+        print(vastus)
+    except:
+        print("Viga")
 
-a=float(input("Arv1: "))
-b=float(input("Arv2: "))
-t=input("Tehe: ")
-vastus=arithmetic(a,b,t)
-print(vastus)
-
-vastus==arithmetic(float(input("Arv1: ")),float(input("Arv2: ")),input("Tehe: "))
-print(vastus)
-
+        vastus==arithmetic(float(input("Arv1: ")),float(input("Arv2: ")),input("Tehe: "))
+        print(vastus)
+#2
 aasta=int(input("Mis aasta tahad kontrollida?"))
 if aasta>0:
     v=is_year_leap(aasta)
@@ -17,3 +23,82 @@ if aasta>0:
         print(f"{aasta} on liigaasta")
     else:
         print(f"{aasta} ei ole liigaasta")
+
+#3
+while True:
+    try:
+
+        S=float(input("Sisesta ruudu külje pikkus: "))
+        if S.lower()=="exit":
+            print("Programm lõpetatud.")
+            break
+        S=float(input("Sisesta ruudu külje pikkus: "))
+        P,S,d=square(S)
+        print(f"Ümbermõõt: {P}, Pindala: {S}, Diagonaal: {d}")
+    except:
+        print("Viga, palun uuesti")
+
+#4
+while True:
+    try:
+        param=int(input("Sisesta kuu-(1-12) või 'exit' lõpetamiseks: "))
+        if param.lower()=="exit":
+            print("Programm lõpetatud.")
+            break
+        param=int(param)
+        print("Aastaaeg: ", season(param))
+    except:
+        print("Viga, sisesta uuesti.")
+
+#5
+while True:
+    try:
+        summa=input("Sisesta algne sissemakse või 'exit' lõpetamiseks: ")
+        if summa.lower()=="exit":
+            print("Programm lõpetatud.")
+            break
+        aastad=input("Sisestaa aastate arv: ")
+        if aastad.lower()=="exit":
+            print("Programm lõpetatud.")
+            break
+        summa=float(summa)
+        aastad=int(aastad)
+        if summa<0 or aastad<0:
+            print("Viga")
+            continue
+        final_amount=deposit(summa,aastad)
+        print(f"Pärast {aastad} aastat on teie kontol: {final_amount} eurot")
+    except:
+        print("Viga")
+
+#6
+while True:
+    try:
+        a=input("Sisesta täisarv: ")
+        if a.lower()=="exit":
+            print("Viga")
+        elif a.lower()=="random":
+            print("Jihuslik arv on algarv: ", is_prime)
+        else:
+            a=int(a)
+            if 1<=a<=1000:
+                print(f"{a} on algarv: {is_prime(a)}")
+            else:
+                print("Viga")
+    except:
+        print("Viga")
+
+
+
+#7
+while True:
+    p=int(input("Sisesta päev: "))
+    k=int(input("Sisesta päev: "))
+    a=int(input("Sisesta päev: "))
+    if date(p,k,a):
+        print("Kuupäev eksisteerib")
+    else:
+        print("Viga")
+    v=input("Kas soovid jätkata? (jah/ei): ")
+    if v.lower()!="jah":
+        break
--- Teema5FunktsioonidDef.py
+++ Teema5FunktsioonidDef.py
@@ -19,7 +19,7 @@ def arithmetic(arv1:float,arv2:float,tehe:str)-> any:
     else:
         vastus="Tundmatu tehe"
     return vastus
-
+#2
 def is_year_leap(aasta:int)->bool:
     """Liigaasta leidmine
     Tagastab True,kui aasta on liigaasta ja False kui aasta on tavaline aasta
@@ -27,6 +27,84 @@ def is_year_leap(aasta:int)->bool:
     :rtype: bool tõeväärsuses formaadis tulemus
     """
     if aasta%4==0:
+        v=True
+    else:
+        v=False
+    return v
+#3
+def square(külg:float)->any:
+    """Funktsioon leiab
+    Ruudu ümbermõõtu
+    Ruudu pindala
+    Ruudu diagonaal
+    :param float S: Sisend ujukomaarv
+    :param float P: Sisesnd ujukomaarv
+    :param float d: Sisesnd ujukomaarv
+    :rtype:tüüp(float)
+    """
+    S=külg**2
+    P=4*külg
+    d=(2)**(1/2)
+    return S,P,d
+#4
+def season(param:int)->str:
+    """Funktsioon võtab kuu numbri (1-12) ja tagastab sellele vastava aastaaja:
+    Talv-(1,2,12)
+    Kevad-(3,4,5)
+    Suvi-(6,7,8)
+    Sügis-(else kuu)
+    :param in range(1,13)
+    :rtype:tüüp talv,kevad,sügis,suvi
+    """
+    if param in [1,2,12]:
+        H="Talv"
+    elif param in [3,4,5]:
+        H="Kevad"
+    elif param in [6,7,8]:
+        H="Suvi"
+    else:
+        H="Sügis"
+    return H
+#5
+def bank(summa:float,aastad:int)->float:
+    """Funktsioon leiab panus
+    Igal aastal suureneb tema panuse suurus 10%
+    :param aasta: aastate arv, mille jooksul raha hoitakse
+    :param summa Esialgne sissemakse(eurodes)
+    :rtype:tüüp (int vüi float)
+    """
+    for aasta in range(aastad):
+        summa*=1.1
+    return summa
+
+#6
+from random import *
+def is_prime(a=randint(1,1000))->bool:
+    """Funktsioon kontrollib, kas antud arv või algarv
+    :param a: Täisarv vahemikus 1 kuni 1000
+    rtype:True, kui arv on algarv, vastasel juhul False
+    """
+    print(a)
+    v=True
+    for jagaja in range(2,a):
+        if a%jagaja==0:
+            v=False
+    return v
+#7
+def date(p:int,k:int,a:int)->bool:
+    """Funktsioon kontrollib, kas sisestatud kuupäev on kalendris olemas
+    :param p: päev(1-31)
+    :param k: kuu(1-12)
+    :param a: aasta
+    :rtype: True, kui kuupäev on kehtiv, vastasel juhul False
+    """
+    if p in range(1,31) and k in [1,3,5,7,8,10,12] and a>0:
+        v=True
+    elif p in range(1,30) and k==2 and is_year_leap(a):
+        v=True
+    elif p in range(1,29) and k==2:
+        v=True
+    elif p in range(1,31) and k in [4,6,9,11] and a>0:
         v=True
     else:
         v=False
