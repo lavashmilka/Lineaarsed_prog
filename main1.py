5c823a4c2f4501e9131e6577444b843af955767f
DOM <DOM@DESKTOP-4TKRQ8F>
2025-05-12 14:25:30 +0300

    .

--- main1.py
+++ main1.py
@@ -28,13 +28,85 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
-from funktsioonid import *
+from kontaktid_fun2 import *
 fail="kontaktid.txt"
-kontaktid =kontaktid_failist(fail)
+kontaktid = loe_failist()
+
+def kuva_kontaktid():
+    tekstikast.delete("1.0", "end")
+    for kontakt in kontaktid:
+        tekstikast.insert("end", f"{kontakt['nimi']}| {kontakt['telefon']} | {kontakt['email']}\n")
+
+def lisa_kontakt_gui():
+    nimi = nimi_entry.get()
+    telefon = telefon_entry.get()
+    email = email_entry.get()
+    if nimi and telefon and email:
+        lisa_kontakt(kontaktid, nimi,telefon,email)
+        salvesta_kontaktid(kontaktid)
+        messagebox.showinfo("Edu","kontakt lisatud.")
+        nimi_entry.delete(0,'end')
+        telefon_entry.delete(0,'end')
+        email_entry.delete(0,'end')
+        kuva_kontaktid()
+    else:
+        messagebox.showwarning("Tühjad väljad","Täida kõik väljad")
+
+def otsi_kontakt_gui():
+    nimi = nimi_entry.get()
+    tulemused=otsi_kontakt(kontaktid, nimi)
+    if tulemused:
+        kontakt=tulemused[0]
+        otsingu_viide.set(kontakt["nimi"])
+        nimi_entry.delete(0,'end')
+        nimi_entry.insert(0, kontakt["nimi"])
+        telefon_entry.delete(0,'end')
+        telefon_entry.insert(0, kontakt["telefon"])
+        email_entry.delete(0,'end')
+        email_entry.insert(0, kontakt["email"])
+        tekstikast.delete("1.0",'end')
+        tekstikast.insert("end", f"Leitud: {kontakt['nimi']} | {kontakt['telefon']} | {kontakt['email']}\n")
+    else:
+        messagebox.showwarning("Ei leitud", "Kontakt puudub.")
+
+def kustuta_kontakt_gui():
+    nimi = nimi_entry.get()
+    if kustuta_kontakt(kontaktid, nimi):
+        salvesta_kontaktid(kontaktid)
+        messagebox.showinfo("Kustutatud", f"'{nimi}' kustutati.")
+        kuva_kontaktid()
+    else:
+        messagebox.showwarning("Ei leitud", "Kontakt puudub.")
+
+def sorteeri_gui():
+    kontaktid_sorted=sorteeri_kontaktid(kontaktid, "nimi")
+    tekstikast.delete("1.0","end")
+    for kontakt in kontaktid_sorted:
+        tekstikast.insert("end", f"{kontakt['nimi']}| {kontakt['telefon']} | {kontakt['email']}\n")
+
+
+def muuda_kontakt_gui():
+    vana_nimi = otsingu_viide.get()
+    uus_nimi = nimi_entry.get()
+    uus_telefon = telefon_entry.get()
+    uus_email = email_entry.get()
+    if vana_nimi and uus_email and uus_telefon and uus_email:
+        õnnestus=muuda_kontakt(kontaktid, vana_nimi, uus_nimi, uus_telefon, uus_email)
+        if õnnestus:
+            salvesta_kontaktid(kontaktid)
+            messagebox.showinfo("Muudetud", f"'{vana_nimi}' andmed on muudetud.")
+            kuva_kontaktid()
+        else:
+            messagebox.showwarning("Tõrge", "Kontakti ei leitud muudatuseks.")
+    else:
+       messagebox.showwarning("Puuduvad andmed", "Palun täida kõik väljad.")
+
+
+
 
 aken=Tk() 
 aken.title("Kontaktid")
-tk.Label(aken,text="Nimi",bg="lightgreen").pack()
+tk.Label(aken,text="Nimi",bg="lightgreen", font=("Calibri",12, "bold italic underline")).pack()
 nimi_entry=tk.Entry(aken)
 nimi_entry.pack()
 
@@ -42,26 +114,25 @@ nuppude_rida=tk.Frame(aken)
 nuppude_rida.pack()
 
 
-tk.Label(aken,text="Telefon",bg="lightgreen").pack()
+tk.Label(aken,text="Telefon",bg="lightgreen", font=("Calibri",12, "bold italic underline")).pack()
 telefon_entry=tk.Entry(aken)
 telefon_entry.pack(pady=5)
 
 
-tk.Label(aken,text="Email",bg="lightgreen").pack()
+tk.Label(aken,text="Email",bg="lightgreen",font=("Calibri",12, "bold italic underline") ).pack()
 email_entry=tk.Entry(aken)
 email_entry.pack()
 
 nuppude_rida=tk.Frame(aken)
-nuppude_rida.pack()
+nuppude_rida.pack(pady=5)
 
-tk.Button(nuppude_rida,text="Lisa kontakt", command=lisa_kontakt,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
-tk.Button(nuppude_rida,text="Kuva kontaktid", command=kuva_kontaktid,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
-tk.Button(nuppude_rida,text="Otsi kontakt", command=otsi_kontakt,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
-tk.Button(nuppude_rida,text="Kustuta kontakt", command=kustuta_kontakt,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
-tk.Button(nuppude_rida,text="Paranda kontakt", command=paranda_kontakt,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
-tk.Button(nuppude_rida,text="Sorteeri kontaktid", command=sorteeri_kontakt,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
-tk.Button(nuppude_rida,text="Saada kiri", command=saada_kiri,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
-tk.Button(nuppude_rida,text="Välju", command=salvesta_kontaktid,bg="lightgreen",font=("Calibri",10)).pack(side="left",pady=5)
+tk.Button(nuppude_rida,text="Lisa kontakt", command=lisa_kontakt,font=("Calibri",12, "bold italic"),bg="lightgreen",fg="black").pack(side="left")
+tk.Button(nuppude_rida,text="Kuva kontaktid", command=kuva_kontaktid,font=("Calibri",12, "bold italic"),bg="lightgreen",fg="black").pack(side="left")
+tk.Button(nuppude_rida,text="Otsi kontakt", command=otsi_kontakt,font=("Calibri",12, "bold italic"),bg="lightgreen",fg="black").pack(side="left")
+tk.Button(nuppude_rida,text="Kustuta kontakt", command=kustuta_kontakt,font=("Calibri",12, "bold italic"),bg="lightgreen",fg="black").pack(side="left")
+tk.Button(nuppude_rida,text="Paranda kontakt", command=paranda_kontakt,font=("Calibri",12, "bold italic"),bg="lightgreen",fg="black").pack(side="left")
+tk.Button(nuppude_rida,text="Sorteeri kontaktid", command=sorteeri_kontakt,font=("Calibri",12, "bold italic"),bg="lightgreen",fg="black").pack(side="left")
+tk.Button(nuppude_rida,text="Välju", command=salvesta_kontaktid,font=("Calibri",12, "bold italic"),bg="lightgreen",fg="black").pack(side="left")
 
 aken.configure(bg="pink")
 aken.iconbitmap("phone.ico")
@@ -88,77 +159,4 @@ aken.mainloop()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# aken.geometry("1200x800")
-# aken.configure(bg="pink")  #bg-background(фон)
-# aken.resizable(width=False, height=False) #akna suuruse muutmine
-# aken.iconbitmap("phone.ico") #akna ikoon
-# #aken.attributes("-alpha", 0.9) #прозрачность окна
-# pealkiri=Label(aken, text="Tere tulemast!\n See on kontaktid",bg="lightblue", font=("Arial", 20))
-# nupp1=Button(aken,text="Lisa kontaktid!",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp1.bind("<Button-1>", lisa_kontakt) 
-# nupp2=Button(aken,text="Kuva kontaktid!",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp2.bind("<Button-1>", kuva_kontaktid) 
-# nupp3=Button(aken,text="Otsi kontaktid",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp3.bind("<Button-1>", otsi_kontakt) 
-# nupp4=Button(aken,text="Kustuta kontaktid!",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp4.bind("<Button-1>", kustuta_kontakt) 
-# nupp5=Button(aken,text="Paranda kontaktid!",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp5.bind("<Button-1>", paranda_kontakt) 
-# nupp6=Button(aken,text="Sorteeri kontaktid!",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp6.bind("<Button-1>", sorteeri_kontakt) 
-# nupp7=Button(aken,text="Sada kiri!",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp7.bind("<Button-1>", saada_kiri)
-# nupp8=Button(aken,text="Välju!",bg="lightgreen",fg="black",font=("Arial",15))
-# nupp8.bind("<Button-1>", salvesta_kontaktid) 
-
-
-
-# pilt=PhotoImage(file="iphone.png").subsample(2,2) #subsample-уменьшение изображения в 2 раза)
-# pilt_label=Label(aken,image=pilt)
-
-
-
-
-
-
-# pealkiri.pack(pady=20)
-# nupp1.pack(pady=20)
-# nupp2.pack(pady=20)
-# nupp3.pack(pady=20)
-# nupp4.pack(pady=20)
-# nupp5.pack(pady=20)
-# nupp6.pack(pady=20)
-# nupp7.pack(pady=20)
-# nupp8.pack(pady=20)
-
-# # pilt_label.pack(pady=20)
-# aken.mainloop()
 
